6a9f7d38a5a2effc5166b3233ac630f0cad6b556
grahamr975 <grahamr975@icloud.com>
2019-11-06 11:15:03 -0500

    Moved logging into functions & added Mailbox list function

--- EWSContactSync.ps1
+++ EWSContactSync.ps1
@@ -51,6 +51,7 @@ Param (
 #---------------------------------------------------------[Initialisations]--------------------------------------------------------
 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+$Global:LogPath = $LogPath
 $ErrorActionPreference = "Stop"
 $VerbosePreference = "Continue"
 
@@ -63,24 +64,14 @@ Import-Module .\EWSContacts\Module\ExchangeContacts.psm1 -Force
 # Import Office 365 Administrator credentials
 $Credential = Import-CliXml -Path $CredentialPath
 
-#-----------------------------------------------------------[Fetch Contact Lists]------------------------------------------------------------
+#-----------------------------------------------------------[Fetch Global Address List & Mailbox List]------------------------------------------------------------
 
 # Fetch list of Global Address List contacts using Office 365 Powershell
-try {
-    $GALContacts = Get-GALContacts -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credentials $Credential
-} catch {
-    Write-Log -Level "FATAL" -Message "Failed to fetch Office 365 Global Address List contacts" -logfile $LogPath
-}
+$GALContacts = Get-GALContacts -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credentials $Credential
 
 # If 'DIRECTORY' is used for $MailboxList, fetch all Mailboxes from the administrator account's Office 365 directory
 if ($MailboxList -eq "DIRECTORY") {
-    try {
-        # TO DO: ADD MAILBOX FETCH FEATURE (IT WILL REPLACE THE BELOW LINE)
-        $MailboxList = $null
-    } catch {
-        Write-Log -Level "FATAL" -Message "Failed to fetch Office 365 mailboxes" -logfile $LogPath
-    }
-    
+        $MailboxList = Get-Mailboxes -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credentials $Credential
 }
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
@@ -99,13 +90,17 @@ foreach ($Mailbox in $MailboxList) {
     $MailboxContacts = Get-EXCContacts -MailboxName $Mailbox -Credentials $Credential -Folder "Contacts\$FolderName" | Where-Object {$_.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1].Address -ne $null}
     $MailboxEmailList = Get-EmailAddressFromContact -Contact $MailboxContacts
 
-    # Determine if each contact needs to be deleted, created, or updated
+    # For each contact, determine if it needs to be deleted, updated, or created
+    # Delete = Any contact in the user's mailbox that does not have an email address matching any contact in the Global Address List
+    # Update = Any contact in the user's mailbox that has a matching email address with a contact in the Global Address List
+    # Create = Any contact in the Global Address List does not does not have a matching email with a contact in the user's mailbox
     $MailboxContactsToBeDeleted = $MailboxContacts | Where-Object {!$GALContacts.WindowsEmailAddress.ToLower().Contains($_.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1].Address.ToLower())}
-    $MailboxContactsToBeCreated = $GALContacts | Where-Object {!$MailboxEmailList.Contains($_.WindowsEmailAddress)}
     $MailboxContactsToBeUpdated = $GALContacts | Where-Object {$MailboxEmailList.Contains($_.WindowsEmailAddress)}
+    $MailboxContactsToBeCreated = $GALContacts | Where-Object {!$MailboxEmailList.Contains($_.WindowsEmailAddress)}
 
-    # ------[DELETE]------
-    # Delete any obsolete contacts (No longer found in the Global Address List)
+    #
+    # DELETE
+    #
     # NOTE: This cannot yet remove contacts with no email address!
     try {
         foreach ($MailboxContactToDelete in $MailboxContactsToBeDeleted) {
@@ -116,14 +111,15 @@ foreach ($Mailbox in $MailboxList) {
     } catch {
         Write-Log -Level "ERROR" -Message "Failed to remove all obsolete contacts from $($Mailbox)'s mailbox"-logfile $LogPath -exception $_.Exception.Message
     }
-    #   ------[UPDATE]------
+    #
+    # UPDATE
+    #
     foreach ($GALContact in $MailboxContactsToBeUpdated) {
-        # Search for a matching contact. If it already exists in the user's mailbox, don't made any changes to it since they aren't needed.
+        # Search for a identical contact. If the identical contact already exists in the user's mailbox, don't made any changes to it since they aren't needed.
         if ($null -eq $($MailboxContacts | Where-Object {(($_.GivenName -eq $GALContact.FirstName) -or ("" -eq $GALContact.FirstName)) -and (($_.Surname -eq $GALContact.LastName) -or ("" -eq $GALContact.LastName)) -and ($_.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1].address -eq $GALContact.WindowsEmailAddress) -and ($GALContact.Company -eq $_.CompanyName -or $GALContact.Company -eq "") -and ($GALContact.Department -eq $_.Department -or $GALContact.Department -eq "") -and (($_.DisplayName -eq $GALContact.DisplayName) -or ($GALContact.DisplayName -eq "")) -and ($GALContact.Title -eq $_.JobTitle -or $GALContact.Title -eq "") -and ($GALContact.Phone -eq $_.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::BusinessPhone] -or $GALContact.Phone -eq "") -and ($GALContact.MobilePhone -eq $_.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::MobilePhone] -or $GALContact.MobilePhone -eq "")})) {
             if ($null -ne $GALContact.WindowsEmailAddress) {
-                Write-Verbose $GALContact
                 Write-Verbose "Updating Contact: $($GALContact.WindowsEmailAddress)"
-                try {   # Try to update the contact if it already exists
+                try {
                     Set-EXCContact -MailboxName $Mailbox -DisplayName $GALContact.DisplayName -FirstName $GALContact.FirstName -LastName $GALContact.LastName -EmailAddress $GALContact.WindowsEmailAddress -CompanyName $GALContact.Company -Credentials $Credential -Department $GALContact.Department -BusinssPhone $GALContact.Phone -MobilePhone $GALContact.MobilePhone -JobTitle $GALContact.Title -Folder "Contacts\$FolderName" -useImpersonation -force
                 } catch {
                     Write-Log -Level "ERROR" -Message "Failed to sync $($GALContact.WindowsEmailAddress) contact to $($Mailbox)'s mailbox" -logfile $LogPath -exception $_.Exception.Message
@@ -131,10 +127,12 @@ foreach ($Mailbox in $MailboxList) {
             }
         }
     }
-    #   ------[CREATE]------
+    #
+    # CREATE
+    #
     foreach ($GALContact in $MailboxContactsToBeCreated) {
         if ($null -ne $GALContact.WindowsEmailAddress) {
-            Write-Verbose "Adding Contact: $($GALContact.WindowsEmailAddress)"
+            Write-Verbose "Creating Contact: $($GALContact.WindowsEmailAddress)"
             try {
                 New-EXCContact -MailboxName $Mailbox -DisplayName $GALContact.DisplayName -FirstName $GALContact.FirstName -LastName $GALContact.LastName -EmailAddress $GALContact.WindowsEmailAddress -CompanyName $GALContact.Company -Credentials $Credential -Department $GALContact.Department -BusinssPhone $GALContact.Phone -MobilePhone $GALContact.MobilePhone -JobTitle $GALContact.Title -Folder "Contacts\$FolderName" -useImpersonation
             } catch {
--- Functions/library.ps1
+++ Functions/library.ps1
@@ -79,7 +79,8 @@ function Get-GALContacts {
 		$Credentials
 	)
 	process {
-		# Connect to Office 365 Exchange Server using a Remote Session
+		try {
+			# Connect to Office 365 Exchange Server using a Remote Session
 		$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $ConnectionUri -Credential $Credentials -Authentication Basic -AllowRedirection
 		Import-PSSession $Session -DisableNameChecking
 		
@@ -91,7 +92,50 @@ function Get-GALContacts {
 			$GALContacts = $GALContacts | Select-Object DisplayName,FirstName,LastName,Title,Company,Department,WindowsEmailAddress,Phone,MobilePhone | Where-Object {$EmailAddressList.Contains($_.WindowsEmailAddress)}
 		Remove-PSSession $Session
 		return $GALContacts
+		} catch {
+			Write-Log -Level "FATAL" -Message "Failed to fetch Global Address List Contacts from Office 365 Directory" -logfile $Global:LogPath -exception $_.Exception.Message
+		}
 	}
+}
+
+function Get-Mailboxes {
+	<#
+	.SYNOPSIS
+		Uses Office 365 services to generate a list of contacts 
+	
+	.PARAMETER ConnectionUri
+		Used to connect to Office 365, by default this is https://outlook.office365.com/powershell-liveid/.
+	
+	.PARAMETER Credentials
+		Office 365 Admin Credentials
+	
+	.EXAMPLE
+		PS C:\> Get-GALContacts -ConnectionUri 'https://outlook.office365.com/powershell-liveid/' -Credentials $Credentials
+	#>
+[CmdletBinding()]
+param (
+	[Parameter(Position = 0, Mandatory = $true)]
+	[string]
+	$ConnectionUri,
+
+	[Parameter(Position = 1, Mandatory = $true)]
+	[System.Management.Automation.PSCredential]
+	$Credentials
+)
+process {
+	try {
+		# Connect to Office 365 Exchange Server using a Remote Session
+	$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $ConnectionUri -Credential $Credentials -Authentication Basic -AllowRedirection
+	Import-PSSession $Session -DisableNameChecking
+	
+		$DirectoryList = $(Get-Mailbox -ResultSize unlimited | Where-Object {$_.HiddenFromAddressListsEnabled -Match "False"})
+		$EmailAddressList = $DirectoryList.PrimarySMTPAddress
+	Remove-PSSession $Session
+	return $EmailAddressList
+	} catch {
+		Write-Log -Level "FATAL" -Message "Failed to fetch user mailbox list from Office 365 directory" -logfile $Global:LogPath -exception $_.Exception.Message
+	}
+}
 }
 
 function Get-EmailAddressFromContact {
