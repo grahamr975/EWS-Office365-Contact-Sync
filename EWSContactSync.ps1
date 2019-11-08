c416afe136c03bebbcd9f1e40762a9316f350885
grahamr975 <grahamr975@icloud.com>
2019-11-08 08:43:04 -0500

    Refactored Sync-ContactList into function

--- EWSContactSync.ps1
+++ EWSContactSync.ps1
@@ -55,6 +55,8 @@ Param (
 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 $Global:LogPath = $LogPath
+$SleepTimer = 500
+$MaxThreads = 5
 $ErrorActionPreference = "Stop"
 $VerbosePreference = "Continue"
 
@@ -74,70 +76,39 @@ $GALContacts = Get-GALContacts -ConnectionUri https://outlook.office365.com/powe
 
 # If 'DIRECTORY' is used for $MailboxList, fetch all Mailboxes from the administrator account's Office 365 directory
 if ($MailboxList -eq "DIRECTORY") {
-        $MailboxList = Get-Mailboxes -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credentials $Credential
+    $MailboxList = Get-Mailboxes -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credentials $Credential | Select-Object PrimarySMTPAddress
 }
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
-foreach ($Mailbox in $MailboxList) {
-    Write-Log -Message "Beginning contact sync for $($Mailbox)'s mailbox" -logfile $LogPath
 
-    # Check if a contacts folder exists with $FolderName. If not, create it.
-    New-EXCContactFolder -MailboxName $Mailbox -FolderName "$FolderName" -Credential $Credential
-    
+# foreach ($Mailbox in $MailboxList) {
+#     Sync-ContactList -Mailbox $Mailbox -Credential $Credential -FolderName $FolderName -ContactList $GALContacts
+# }
 
-    # Fetch contacts from the user's mailbox
-    $MailboxContacts = Get-EXCContacts -MailboxName $Mailbox -Credentials $Credential -Folder "Contacts\$FolderName" | Where-Object {$_.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1].Address -ne $null}
-    $MailboxEmailList = Get-EmailAddressFromContact -Contact $MailboxContacts
+ForEach ($Mailbox in $MailboxList) {
 
-    # For each contact, determine if it needs to be deleted, updated, or created
-    # Delete = Any contact in the user's mailbox that does not have an email address matching any contact in the Global Address List
-    # Update = Any contact in the user's mailbox that has a matching email address with a contact in the Global Address List
-    # Create = Any contact in the Global Address List does not does not have a matching email with a contact in the user's mailbox
-    $MailboxContactsToBeDeleted = $MailboxContacts | Where-Object {!$GALContacts.WindowsEmailAddress.ToLower().Contains($_.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1].Address.ToLower())}
-    $MailboxContactsToBeUpdated = $GALContacts | Where-Object {$MailboxEmailList.Contains($_.WindowsEmailAddress)}
-    $MailboxContactsToBeCreated = $GALContacts | Where-Object {!$MailboxEmailList.Contains($_.WindowsEmailAddress)}
-
-    #
-    # DELETE
-    #
-    # NOTE: This cannot yet remove contacts with no email address!
-    try {
-        foreach ($MailboxContactToDelete in $MailboxContactsToBeDeleted) {
-            Write-Verbose "Deleting Contact: $($MailboxContactToDelete.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1].Address.ToLower())"
-            $MailboxContactToDelete.Delete([Microsoft.Exchange.WebServices.Data.DeleteMode]::SoftDelete)
-        }
-        # Write-Log -Message "Removed all obsolete contacts from $($Mailbox)'s mailbox" -logfile $LogPath
-    } catch {
-        Write-Log -Level "ERROR" -Message "Failed to remove all obsolete contacts from $($Mailbox)'s mailbox"-logfile $LogPath -exception $_.Exception.Message
+    While ($(Get-Job -state running).count -ge $MaxThreads){
+        Write-Progress  -Activity "Syncing Contacts for Office 365 Mailbox" -Status "Waiting for Contact Sync to Finish..." -CurrentOperation "$i Contact Sync Jobs created - $($(Get-Job -state running).count) jobs running" -PercentComplete ($i / $MailboxList.count * 100)
+        Start-Sleep -Milliseconds $SleepTimer
     }
-    #
-    # UPDATE
-    #
-    foreach ($GALContact in $MailboxContactsToBeUpdated) {
-        # Search for a identical contact. If the identical contact already exists in the user's mailbox, don't made any changes to it since they aren't needed.
-        if ($null -eq $($MailboxContacts | Where-Object {(($_.GivenName -eq $GALContact.FirstName) -or ("" -eq $GALContact.FirstName)) -and (($_.Surname -eq $GALContact.LastName) -or ("" -eq $GALContact.LastName)) -and ($_.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1].address -eq $GALContact.WindowsEmailAddress) -and ($GALContact.Company -eq $_.CompanyName -or $GALContact.Company -eq "") -and ($GALContact.Department -eq $_.Department -or $GALContact.Department -eq "") -and (($_.DisplayName -eq $GALContact.DisplayName) -or ($GALContact.DisplayName -eq "")) -and ($GALContact.Title -eq $_.JobTitle -or $GALContact.Title -eq "") -and ($GALContact.Phone -eq $_.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::BusinessPhone] -or $GALContact.Phone -eq "") -and ($GALContact.MobilePhone -eq $_.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::MobilePhone] -or $GALContact.MobilePhone -eq "")})) {
-            if ($null -ne $GALContact.WindowsEmailAddress) {
-                Write-Verbose "Updating Contact: $($GALContact.WindowsEmailAddress)"
-                try {
-                    Set-EXCContact -MailboxName $Mailbox -DisplayName $GALContact.DisplayName -FirstName $GALContact.FirstName -LastName $GALContact.LastName -EmailAddress $GALContact.WindowsEmailAddress -CompanyName $GALContact.Company -Credentials $Credential -Department $GALContact.Department -BusinssPhone $GALContact.Phone -MobilePhone $GALContact.MobilePhone -JobTitle $GALContact.Title -Folder "Contacts\$FolderName" -useImpersonation -force
-                } catch {
-                    Write-Log -Level "ERROR" -Message "Failed to sync $($GALContact.WindowsEmailAddress) contact to $($Mailbox)'s mailbox" -logfile $LogPath -exception $_.Exception.Message
-                }
-            }
-        }
-    }
-    #
-    # CREATE
-    #
-    foreach ($GALContact in $MailboxContactsToBeCreated) {
-        if ($null -ne $GALContact.WindowsEmailAddress) {
-            Write-Verbose "Creating Contact: $($GALContact.WindowsEmailAddress)"
-            try {
-                New-EXCContact -MailboxName $Mailbox -DisplayName $GALContact.DisplayName -FirstName $GALContact.FirstName -LastName $GALContact.LastName -EmailAddress $GALContact.WindowsEmailAddress -CompanyName $GALContact.Company -Credentials $Credential -Department $GALContact.Department -BusinssPhone $GALContact.Phone -MobilePhone $GALContact.MobilePhone -JobTitle $GALContact.Title -Folder "Contacts\$FolderName" -useImpersonation
-            } catch {
-                Write-Log -Level "ERROR" -Message "Failed to create $($GALContact.WindowsEmailAddress) contact in $($Mailbox)'s mailbox" -logfile $LogPath -exception $_.Exception.Message
-            }
-        }
-    }
+
+	$i++
+    Start-Job -ScriptBlock ${function:Sync-ContactList} -ArgumentList $Mailbox, $Credential, $FolderName, $GALContacts -Name $Mailbox  | Out-Null
+    Write-Progress  -Activity "Syncing Contacts for Office 365 Mailbox" -Status "Starting Threads" -CurrentOperation "$i Contact Sync started - $($(Get-Job -state running).count) jobs running" -PercentComplete ($i / $MailboxList.count * 100)
+}
+
+# Show Progress on All Running Threads
+While ($(Get-Job -State Running).count -gt 0){
+    $ThreadsStillRunning = ""
+    ForEach ($System  in $(Get-Job -state running)){$ThreadsStillRunning += ", $($System.name)"}
+    $ThreadsStillRunning = $ThreadsStillRunning.Substring(2)
+    Write-Progress  -Activity "Syncing Contacts for Office 365 Mailbox" -Status "$($(Get-Job -State Running).count) Sync(s) remaining" -CurrentOperation "$ThreadsStillRunning" -PercentComplete ($(Get-Job -State Completed).count / $(Get-Job).count * 100)
+    Start-Sleep -Milliseconds $SleepTimer
+}
+
+ForEach($Job in Get-Job) {
+	$JobResults = $(Receive-Job $Job)
+        #Add-content $LogPath -value $JobResults
+        Write-Log -Message $JobResults -LogPath $LogPath
 }
--- Functions/library.ps1
+++ Functions/library.ps1
@@ -52,7 +52,7 @@ function New-EXCContactFolder
 				$ContactsFolder = [Microsoft.Exchange.WebServices.Data.ContactsFolder]::Bind($service,$ContactsFolderSearch.Id);
 			}
 		} catch {
-			Write-Log -Level "FATAL" -Message "Failed verify that $($FolderName) exists for $($Mailbox)" -logfile $Global:LogPath -exception $_.Exception.Message
+			Write-Log -Level "FATAL" -Message "Failed verify that $($FolderName) exists for $($Mailbox)" -exception $_.Exception.Message
 		}
     }
 }
@@ -92,12 +92,12 @@ function Get-GALContacts {
 			$EmailAddressList = $DirectoryList.PrimarySMTPAddress
 		
 			# Import Global Address List into Powershell from Office 365 Exchange as an array
-			$GALContacts = Get-User -ResultSize unlimited | Where-Object {$null -ne $_.WindowsEmailAddress}
-			$GALContacts = $GALContacts | Select-Object DisplayName,FirstName,LastName,Title,Company,Department,WindowsEmailAddress,Phone,MobilePhone | Where-Object {$EmailAddressList.Contains($_.WindowsEmailAddress)}
+			$ContactList = Get-User -ResultSize unlimited | Where-Object {$null -ne $_.WindowsEmailAddress}
+			$ContactList = $ContactList | Select-Object DisplayName,FirstName,LastName,Title,Company,Department,WindowsEmailAddress,Phone,MobilePhone | Where-Object {$EmailAddressList.Contains($_.WindowsEmailAddress)}
 		Remove-PSSession $Session
-		return $GALContacts
+		return $ContactList
 		} catch {
-			Write-Log -Level "FATAL" -Message "Failed to fetch Global Address List Contacts from Office 365 Directory" -logfile $Global:LogPath -exception $_.Exception.Message
+			Write-Log -Level "FATAL" -Message "Failed to fetch Global Address List Contacts from Office 365 Directory" -exception $_.Exception.Message
 		}
 	}
 }
@@ -137,7 +137,7 @@ process {
 	Remove-PSSession $Session
 	return $EmailAddressList
 	} catch {
-		Write-Log -Level "FATAL" -Message "Failed to fetch user mailbox list from Office 365 directory" -logfile $Global:LogPath -exception $_.Exception.Message
+		Write-Log -Level "FATAL" -Message "Failed to fetch user mailbox list from Office 365 directory" -exception $_.Exception.Message
 	}
 }
 }
@@ -163,60 +163,159 @@ process {
 	}
 }
 
-
 Function Write-Log {
-		<#
-		.SYNOPSIS
-			Writes the specified message to the screen. If $logfile is specified, write to the file instead.
-		
-		.PARAMETER Level
-			Indicates the type of message. The options are INFO, WARN, ERROR, FATAL, and DEBUG. Uses INFO by default.
-		
-		.PARAMETER Message
-			Message to write to the screen or the $logfile (if specified)
+	<#
+	.SYNOPSIS
+		Writes the specified message to the screen. If $logfile is specified, write to the file instead.
+	
+	.PARAMETER Level
+		Indicates the type of message. The options are INFO, WARN, ERROR, FATAL, and DEBUG. Uses INFO by default.
+	
+	.PARAMETER Message
+		Message to write to the screen or the $logfile (if specified)
 
-		.PARAMETER logfile
-			Optional; File path of the file to write the message to.
+	.PARAMETER logfile
+		Optional; File path of the file to write the message to.
 
-		.PARAMETER exception
-			Optional; Additional information to include when the script encounters an error. Very useful for troubleshooting.
-		
-		.EXAMPLE
-			PS C:\> Write-Log -Level "INFO" -Message "Successfully executed the script" -logfile $LogFilePath
+	.PARAMETER exception
+		Optional; Additional information to include when the script encounters an error. Very useful for troubleshooting.
+	
+	.EXAMPLE
+		PS C:\> Write-Log -Level "INFO" -Message "Successfully executed the script" -logfile $LogFilePath
+#>
+[CmdletBinding()]
+Param(
+[Parameter(Mandatory=$False)]
+[ValidateSet("INFO","WARN","ERROR","FATAL","DEBUG")]
+[String]
+$Level = "INFO",
+
+[Parameter(Mandatory=$True)]
+[string]
+$Message,
+
+[Parameter(Mandatory=$False)]
+[string]
+$logfile,
+
+[Parameter(Mandatory=$False)]
+[string]
+$exception
+)
+
+if ($logfile -and !(Test-Path $logfile)) {
+	New-Item -ItemType "file" -Path "$logfile" -Force | Out-Null
+  }
+
+$Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+$Line = "$Stamp $Level $Message $exception"
+If($logfile) {
+	Add-Content $logfile -Value $Line -Force
+}
+Else {
+	Write-Output $Line
+}
+if ($Level -eq "FATAL") {
+	Write-Error $Message
+}
+}
+
+function Sync-ContactList {
+	<#
+	.SYNOPSIS
+		Uses Exchange Web Services API to syncronise a list of contacts to an Office 365 Mailbox
+	
+	.PARAMETER Mailbox
+		Email address of an Office 365 user
+	
+	.PARAMETER Credential
+		Office 365 Admin Credentials
+
+	.PARAMETER FolderName
+		Name of the contact folder that will be synced to
+
+	.PARAMETER ContactList
+		An array with DisplayName, FirstName, LastName, Title, Company, Department, WindowsEmailAddress, Phone, and MobilePhone properties
+	
+	.EXAMPLE
+		PS C:\> Sync-ContactList -Mailbox "john.doe@example.com" -Credential $Credentials -FolderName "myFolder" -ContactList $Contacts
 	#>
-    [CmdletBinding()]
-    Param(
-    [Parameter(Mandatory=$False)]
-    [ValidateSet("INFO","WARN","ERROR","FATAL","DEBUG")]
-    [String]
-    $Level = "INFO",
+[CmdletBinding()]
+param (
+	[Parameter(Position = 0, Mandatory = $true)]
+	[string]
+	$Mailbox,
 
-    [Parameter(Mandatory=$True)]
-    [string]
-    $Message,
+	[Parameter(Position = 1, Mandatory = $true)]
+	[System.Management.Automation.PSCredential]
+	$Credential,
 
-    [Parameter(Mandatory=$False)]
-    [string]
-	$logfile,
-	
-	[Parameter(Mandatory=$False)]
-    [string]
-	$exception
-	)
-	
-	if ($logfile -and !(Test-Path $logfile)) {
-		New-Item -ItemType "file" -Path "$logfile" -Force | Out-Null
-	  }
+	[Parameter(Position = 2, Mandatory = $true)]
+	[string]
+	$FolderName,
 
-    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
-    $Line = "$Stamp $Level $Message $exception"
-    If($logfile) {
-		Add-Content $logfile -Value $Line -Force
+	[Parameter(Position = 3, Mandatory = $true)]
+	$ContactList
+)
+process {
+	Write-Log -Message "Beginning contact sync for $($Mailbox)'s mailbox"
+
+	# Check if a contacts folder exists with $FolderName. If not, create it.
+	New-EXCContactFolder -MailboxName $Mailbox -FolderName "$FolderName" -Credential $Credential
+
+
+	# Fetch contacts from the user's mailbox
+	$MailboxContacts = Get-EXCContacts -MailboxName $Mailbox -Credentials $Credential -Folder "Contacts\$FolderName" | Where-Object {$null -ne $_.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1].Address}
+	$MailboxEmailList = Get-EmailAddressFromContact -Contact $MailboxContacts
+
+	# For each contact, determine if it needs to be deleted, updated, or created
+	# Delete = Any contact in the user's mailbox that does not have an email address matching any contact in the Global Address List
+	# Update = Any contact in the user's mailbox that has a matching email address with a contact in the Global Address List
+	# Create = Any contact in the Global Address List does not does not have a matching email with a contact in the user's mailbox
+	$MailboxContactsToBeDeleted = $MailboxContacts | Where-Object {!$ContactList.WindowsEmailAddress.ToLower().Contains($_.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1].Address.ToLower())}
+	$MailboxContactsToBeUpdated = $ContactList | Where-Object {$MailboxEmailList.Contains($_.WindowsEmailAddress)}
+	$MailboxContactsToBeCreated = $ContactList | Where-Object {!$MailboxEmailList.Contains($_.WindowsEmailAddress)}
+
+	#
+	# DELETE
+	#
+	# NOTE: This cannot yet remove contacts with no email address!
+	try {
+		foreach ($Contact in $MailboxContactsToBeDeleted) {
+			Write-Verbose "Deleting Contact: $($Contact.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1].Address.ToLower())"
+			$Contact.Delete([Microsoft.Exchange.WebServices.Data.DeleteMode]::SoftDelete)
+		}
+	} catch {
+		Write-Log -Level "ERROR" -Message "Failed to remove all obsolete contacts from $($Mailbox)'s mailbox" -exception $_.Exception.Message
 	}
-    Else {
-        Write-Output $Line
+	#
+	# UPDATE
+	#
+	foreach ($Contact in $MailboxContactsToBeUpdated) {
+		# Search for a identical contact. If the identical contact already exists in the user's mailbox, don't made any changes to it since they aren't needed.
+		if ($null -eq $($MailboxContacts | Where-Object {(($_.GivenName -eq $Contact.FirstName) -or ("" -eq $Contact.FirstName)) -and (($_.Surname -eq $Contact.LastName) -or ("" -eq $Contact.LastName)) -and ($_.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1].address -eq $Contact.WindowsEmailAddress) -and ($Contact.Company -eq $_.CompanyName -or $Contact.Company -eq "") -and ($Contact.Department -eq $_.Department -or $Contact.Department -eq "") -and (($_.DisplayName -eq $Contact.DisplayName) -or ($Contact.DisplayName -eq "")) -and ($Contact.Title -eq $_.JobTitle -or $Contact.Title -eq "") -and ($Contact.Phone -eq $_.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::BusinessPhone] -or $Contact.Phone -eq "") -and ($Contact.MobilePhone -eq $_.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::MobilePhone] -or $Contact.MobilePhone -eq "")})) {
+			if ($null -ne $Contact.WindowsEmailAddress) {
+				Write-Verbose "Updating Contact: $($Contact.WindowsEmailAddress)"
+				try {
+					Set-EXCContact -MailboxName $Mailbox -DisplayName $Contact.DisplayName -FirstName $Contact.FirstName -LastName $Contact.LastName -EmailAddress $Contact.WindowsEmailAddress -CompanyName $Contact.Company -Credentials $Credential -Department $Contact.Department -BusinssPhone $Contact.Phone -MobilePhone $Contact.MobilePhone -JobTitle $Contact.Title -Folder "Contacts\$FolderName" -useImpersonation -force
+				} catch {
+					Write-Log -Level "ERROR" -Message "Failed to sync $($Contact.WindowsEmailAddress) contact to $($Mailbox)'s mailbox" -exception $_.Exception.Message
+				}
+			}
+		}
 	}
-	if ($Level -eq "FATAL") {
-		Write-Error $Message
+	#
+	# CREATE
+	#
+	foreach ($Contact in $MailboxContactsToBeCreated) {
+		if ($null -ne $Contact.WindowsEmailAddress) {
+			Write-Verbose "Creating Contact: $($Contact.WindowsEmailAddress)"
+			try {
+				New-EXCContact -MailboxName $Mailbox -DisplayName $Contact.DisplayName -FirstName $Contact.FirstName -LastName $Contact.LastName -EmailAddress $Contact.WindowsEmailAddress -CompanyName $Contact.Company -Credentials $Credential -Department $Contact.Department -BusinssPhone $Contact.Phone -MobilePhone $Contact.MobilePhone -JobTitle $Contact.Title -Folder "Contacts\$FolderName" -useImpersonation
+			} catch {
+				Write-Log -Level "ERROR" -Message "Failed to create $($Contact.WindowsEmailAddress) contact in $($Mailbox)'s mailbox" -exception $_.Exception.Message
+			}
+		}
 	}
+}
 }
