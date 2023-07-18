384df026e47f70d8e7b58f16b93bd07577c8b030
grahamr975 <grahamr975@icloud.com>
2023-07-18 13:33:40 -0400

    ExchangeOnlineManagement version limits #73, fix for Expired or Invalid pagination request

--- EWSContacts/Module/functions/contacts/Get-Mailboxes.ps1
+++ EWSContacts/Module/functions/contacts/Get-Mailboxes.ps1
@@ -39,7 +39,7 @@ process {
 	# $Null = @() is a workaround for this function returning a random filename such as "tmp_z1ci55dv.kke" at the start of the output....
 	$Null = @(
 		# Connect to Office 365 Exchange Server using a Remote Session
-	Connect-ExchangeOnline -ConnectionUri $ConnectionUri -CertificateFilePath $CertificatePath -CertificatePassword $CertificatePassword -AppId $ClientID -Organization $ExchangeOrg
+	Connect-ExchangeOnline -CertificateFilePath $CertificatePath -CertificatePassword $CertificatePassword -AppId $ClientID -Organization $ExchangeOrg
 			$DirectoryList = $(Get-EXOMailbox -ResultSize unlimited | Where-Object {$_.HiddenFromAddressListsEnabled -Match "False"}).PrimarySMTPAddress
     Disconnect-ExchangeOnline -Confirm:$false
 	)
